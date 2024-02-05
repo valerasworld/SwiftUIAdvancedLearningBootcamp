f2ab7cecdcd28f71e543c43a964a395fcbd61614
Valery Zazulin <zazulinvalerii@gmail.com>
2024-02-05 23:29:54 +0200

    Initial Commit

---
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingAdvancedLearningApp.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by Валерий Зазулин on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
