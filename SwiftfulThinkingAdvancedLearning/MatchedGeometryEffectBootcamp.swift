7f402a463715976c1668ef026e64566621454240
Valery Zazulin <zazulinvalerii@gmail.com>
2024-02-11 01:42:44 +0100

    4. MatchedGeometryEffect

---
+++ SwiftfulThinkingAdvancedLearning/MatchedGeometryEffectBootcamp.swift
@@ -0,0 +1,76 @@
+//
+//  MatchedGeometryEffectBootcamp.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by Валерий Зазулин on 11.02.2024.
+//
+
+import SwiftUI
+
+struct MatchedGeometryEffectBootcamp: View {
+    
+    @State private var isClicked: Bool = false
+    @Namespace private var namespace
+    
+    var body: some View {
+        VStack {
+            if !isClicked {
+                Circle()
+                    .matchedGeometryEffect(id: "rectangle", in: namespace)
+                    .frame(width: 100, height: 100)
+            }
+            
+            Spacer()
+            
+            if isClicked {
+                Circle()
+                    .matchedGeometryEffect(id: "rectangle", in: namespace)
+                    .frame(width: 300, height: 200)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .onTapGesture {
+            withAnimation(.easeInOut) {
+                isClicked.toggle()
+            }
+        }
+    }
+}
+
+#Preview {
+    MatchedGeometryEffectExample2()
+}
+
+struct MatchedGeometryEffectExample2: View {
+    
+    let categories: [String] = ["Home", "Popular", "Saved"]
+    @State private var selected: String = ""
+    @Namespace private var namespace2
+    
+    var body: some View {
+        HStack {
+            ForEach(categories, id: \.self) { category in
+                ZStack(alignment: .bottom) {
+                    if selected == category {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.red.opacity(0.5))
+                            .matchedGeometryEffect(id: "categoryBackground", in: namespace2)
+                            .frame(width: 35, height: 2)
+                            .offset(y: 10)
+                    }
+                    Text(category)
+                        .foregroundStyle(selected == category ? Color.red : Color.black)
+                }
+                .frame(maxWidth: .infinity)
+                .frame(height: 55)
+                .onTapGesture {
+                    withAnimation(.spring) {
+                        selected = category
+                    }
+                }
+                
+            }
+        }
+        .padding()
+    }
+}
